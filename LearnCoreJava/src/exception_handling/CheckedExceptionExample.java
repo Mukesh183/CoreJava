913897ec8c5d2de27671c4191ce54bacec36e9fd
Mukesh183 <mukesh.perumal00@gmail.com>
2023-08-31 08:58:01 +0530

    Exception Commit

---
+++ LearnCoreJava/src/exception_handling/CheckedExceptionExample.java
@@ -0,0 +1,13 @@
+package exception_handling;
+
+public class CheckedExceptionExample {
+	
+	static String name="Mukesh";
+
+	public static void main(String[] args) {
+		//CheckedExceptionExample example = new CheckedExceptionExample();
+		System.out.println(name);
+
+	}
+
+}
---
+++ LearnCoreJava/src/exception_handling/UncheckedExceptionExample.java
@@ -0,0 +1,10 @@
+package exception_handling;
+
+public class UncheckedExceptionExample {
+	static String name;
+	
+	public static void main(String[] args) {
+		System.out.println(name.charAt(0));
+	}
+
+}
